f0ed45ea4381b49e4f2f527681307ddaa597ccae
Vinini Wasanthakumar <kohi.samy@gmail.com>
2024-04-09 00:05:53 +0100

    unit test added

--- src/test/java/com/service/countryInfoConsumer/service/CountryInfoServiceTest.java
+++ src/test/java/com/service/countryInfoConsumer/service/CountryInfoServiceTest.java
@@ -1,50 +1,50 @@
 package com.service.countryInfoConsumer.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.service.countryInfoConsumer.dto.CountryInfoDTO;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.http.client.MockClientHttpResponse;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@SpringBootTest
 class CountryInfoServiceTest {
-
-    @MockBean
-    private WebClient webClient;
-
     @Autowired
     private CountryInfoService countryInfoService;
 
-//    @Test
-//    void testGetCountryInfo() throws JsonProcessingException {
-//        String responseJson = "[{\"name\":{\"common\":\"Finland\"},\"flags\":{\"svg\":\"https://flagcdn.com/fi.svg\"},\"capital\":[\"Helsinki\"],\"cca2\":\"FI\",\"population\":5530719}]";
-//
-//
-//        when(webClient.get()).thenReturn(Mockito.mock(WebClient.RequestHeadersSpec.class));
-//
-//
-//        CountryInfoDTO countryInfoDTO = countryInfoService.getCountryInfo("Finland", "name,flags,capital,cca2,population");
-//
-//        // Verify that the expected data is extracted from the response
-//        assertEquals("Finland", countryInfoDTO.getName());
-//        assertEquals("https://flagcdn.com/fi.svg", countryInfoDTO.getFlagFileUrl());
-//        assertEquals("Helsinki", countryInfoDTO.getCapital());
-//        assertEquals("FI", countryInfoDTO.getCountryCode());
-//        assertEquals(5530719, countryInfoDTO.getPopulation());
-//    }
+    static WireMockServer wireMockServer;
+
+   @BeforeAll
+    public static void setUp(){
+
+       wireMockServer = new WireMockServer();
+       wireMockServer.start();
+       configureFor("localhost", 8080);
+
+       wireMockServer.stubFor(get(urlEqualTo("/name/Finland"))
+               .willReturn(aResponse()
+                       .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                       .withBody("[{\"name\":{\"common\":\"Finland\"},\"flags\":{\"svg\":\"https://flagcdn.com/fi.svg\"},\"capital\":[\"Helsinki\"],\"cca2\":\"FI\",\"population\":5530719}]"))
+       );
+
+    }
+    @Test
+    void testGetCountryInfo() throws JsonProcessingException {
+
+        CountryInfoDTO countryInfoDTO = countryInfoService.getCountryInfo("Finland", "name,flags,capital,cca2,population");
+
+        // Verify that the expected data is extracted from the response
+        assertEquals("Finland", countryInfoDTO.getName());
+        assertEquals("https://flagcdn.com/fi.svg", countryInfoDTO.getFlagFileUrl());
+        assertEquals("Helsinki", countryInfoDTO.getCapital());
+        assertEquals("FI", countryInfoDTO.getCountryCode());
+        assertEquals(5530719, countryInfoDTO.getPopulation());
+    }
 
 }
